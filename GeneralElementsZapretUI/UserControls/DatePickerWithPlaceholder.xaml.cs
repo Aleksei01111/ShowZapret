5641f25d7094751fd055141d73f52c1963dc1a4b
aleksei01111 <alekseivolkov01012@gmail.com>
2026-04-12 16:01:04 +0300

    написал элемент DatePickerWithPlaceholder

---
+++ GeneralElementsZapretUI/UserControls/DatePickerWithPlaceholder.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GeneralElementsZapretUI.UserControls;
+
+public partial class DatePickerWithPlaceholder : UserControl
+{
+    public static readonly DependencyProperty PlaceholderProperty = 
+        DependencyProperty.Register(
+            nameof(Placeholder),
+            typeof(string),
+            typeof(DatePickerWithPlaceholder));
+
+    public static readonly DependencyProperty DateProperty =
+        DependencyProperty.Register(
+            nameof(SelectedDate),
+            typeof(DateTime),
+            typeof(DatePickerWithPlaceholder));
+    
+    public string Placeholder
+    {
+        get => (string)GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+
+    public DateTime SelectedDate
+    {
+        get => (DateTime)GetValue(DateProperty);
+        set => SetValue(DateProperty, value);
+    }
+    
+    public DatePickerWithPlaceholder()
+    {
+        InitializeComponent();
+    }
+}
